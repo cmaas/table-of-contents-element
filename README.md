648a21ccfe1d7e05f7e0e542847920930732e920
Chris Maas <gitlab@cmaas.de>
2024-10-23 16:48:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,137 @@
+# table-of-contents-element
+
+```html
+<table-of-contents></table-of-contents>
+```
+
+A lightweight **Table of contents** generator, distributed as a web component.
+
+**Features:**
+
+* Takes all headlines of a webpage or article and turns them into semantically correct nested lists
+* Selection of headlines can be specified by providing the `selector` attribute, which uses `querySelectorAll`
+* Handles complex cases: headlines not ordered as expected, but h3 followed by h2 etc.
+* Does NOT use shadow DOM for easy styling and to inherit the style of your page/article; does **not** come with its own style
+* Adds links to list items, if a headline has an `id` attribute
+* Can be provided with custom HTML to add a header, footer or wrap the entire list in a `<details>` tag (see Examples)
+* Zero-dependencies
+
+## Usage
+
+Via npm:
+
+```
+npm install table-of-contents-element
+```
+
+```
+import 'table-of-contents-element';
+```
+
+Or as a `<script>` tag:
+
+```
+<script type="module" src="https://cdn.jsdelivr.net/npm/table-of-contents-element/index.js"></script>
+```
+
+Then use the HTML:
+
+```html
+<table-of-contents></table-of-contents>
+```
+
+Including the script automatically defines the custom element.
+
+### Examples
+
+#### Basic usage, finds all `h1`, `h2`, `h3`, `h4` of a page:
+
+```html
+<table-of-contents></table-of-contents>
+```
+
+#### Custom headline selector:
+
+```html
+<table-of-contents selector="article h2, article h3:not(.newsletter-signup)></table-of-contents>
+```
+
+Or target only headlines that have an `id` attribute:
+
+```html
+<table-of-contents selector="h1[id], h2[id], h3[id], h4[id]"></table-of-contents>
+```
+
+#### Change list type to `ol`:
+
+```html
+<table-of-contents listtype="ol"></table-of-contents>
+```
+
+#### Wrap the list in custom HTML:
+
+You can give any child element an attribute called `data-toc-render-target` to specify where the list items should be rendered. This way, you can wrap the table of contents in a `<detail>` tag:
+
+```html
+<table-of-contents>
+    <details>
+        <summary>Table of contents</summary>
+        <div data-render-target></div>
+    </details>
+</table-of-contents>
+```
+
+Or add a `<header>` and `<footer>` to your table of contents:
+
+```
+<table-of-contents>
+    <header>Table of contents</header>
+    <div data-toc-render-target></div>
+    <footer>powered by table-of-contents-element</footer>
+</table-of-contents>
+```
+
+If you do not provide an attribute `data-toc-render-target`, the generated list will be added as the last child:
+
+```
+<table-of-contents>
+    <header>Table of contents</header>
+    <!-- toc list will be placed here -->
+</table-of-contents>
+```
+
+## API
+
+If you need to re-create the table of contents, for example, because the article content changed dynamically, you can call `render()`:
+
+```js
+const toc = document.getElementsByTagName('table-of-contents')[0];
+toc.render(); // recreates the TOC
+```
+
+The `table-of-contents` custom element can be created via JS:
+
+```js
+const toc = document.createElement('table-of-contents');
+toc.setAttribute('selector', 'h2, h3, h4');
+document.body.appendChild(toc);
+```
+
+Please note that `<table-of-contents>` does not observe changes to attributes. In the rare case that you need to change attributes, simply destroy the existing element and re-create it.
+
+### Methods
+
+| Method | Description |
+|:--|:--|
+| `render()` | Recreates the table of contents |
+
+### Attributes
+
+| Attribute | Values | Default | Description |
+|:--|:--|:--|:--|
+| `listtype` | "ul" or "ol" | "ul" | List type for items
+| `selector` | whatever [querySelectorAll()](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll) accepts | "h1, h2, h3, h4" | Query selector for headlines to generate the TOC from
+
+### Properties
+
+None. *Note: Attributes are not reflected to properties.*
---
+++ index.js
@@ -0,0 +1,175 @@
+//@ts-check
+/**
+ * @typedef {Object} HeadlineItem
+ * @property {number} level Headline level
+ * @property {string} id Id in document (anchor target)
+ * @property {string} text Text of headline
+ */
+
+/**
+ * @typedef {Object} TocItem
+ * @property {number} level Item level
+ * @property {string} text Text of link
+ * @property {string} anchor Target of link
+ * @property {Array<TocItem>} children Sub-items for this list item
+ * @property {TocItem} parent Parent this item belongs to
+ */
+
+/**
+ * Uses querySelectorAll to find headline elements.
+ * @example
+ * findHeadlineElements('h1, h2, h3')
+ * @param {string} query Query string for querySelectorAll
+ * @param {Document} doc Document
+ * @returns {Array<HeadlineItem>}
+ */
+function findHeadlineElements(query, doc = document) {
+    return Array.from(doc.querySelectorAll(query)).map((el) => ({
+        level: parseInt(el.tagName.toLowerCase().replace('h', ''), 10),
+        text: el.textContent,
+        id: el.id
+    }));
+}
+
+/**
+ * Helper to get minimum headline level so that the TOC is nested correctly
+ * @param {Array<HeadlineItem>} headlineItems Search these
+ * @returns {number} Minimum level
+ */
+function getMinLevel(headlineItems) {
+    return Math.min(...headlineItems.map(item => item.level));
+}
+
+/**
+ * Helper that creates a TOCItem
+ * @param {number} level
+ * @param {string} text
+ * @param {string} anchor
+ * @param {TocItem} rootNode
+ * @returns {TocItem}
+ */
+function addListItem(level, text, anchor, rootNode) {
+    const listItem = { level, text, anchor, children: [], parent: rootNode };
+    rootNode.children.push(listItem);
+    return listItem;
+}
+
+/**
+ * Turns a list of flat headline items into a nested tree object representing the TOC
+ * @param {Array<HeadlineItem>} headlineItems
+ * @returns {TocItem} Tree of TOC items
+ */
+function flatHeadlineItemsToNestedTree(headlineItems) {
+    // create a root node with no text that holds the entire TOC. this won't be rendered, but only its children
+    const toc = { level: getMinLevel(headlineItems) - 1, anchor: null, text: null, children: [], parent: null };
+    // pointer that tracks the last root item of the current list
+    let currentRootNode = toc;
+    // pointer that tracks the last item (to turn it into a new root node if necessary)
+    let prevListItem = currentRootNode;
+
+    headlineItems.forEach(headlineItem => {
+        if (headlineItem.level > prevListItem.level) {
+            console.log('level up', headlineItem.text, currentRootNode.text, headlineItem.level - prevListItem.level);
+            Array.from({ length: headlineItem.level - prevListItem.level }).forEach(_ => {
+                currentRootNode = prevListItem;
+                prevListItem = addListItem(headlineItem.level, null, null, currentRootNode);
+            });
+            prevListItem.text = headlineItem.text;
+            prevListItem.anchor = headlineItem.id;
+            // if level is bigger, take the previous node, add a child list, set current list to this new child list
+        }
+        else if (headlineItem.level === prevListItem.level) {
+            console.log('same level', headlineItem.text, currentRootNode.text);
+            prevListItem = addListItem(headlineItem.level, headlineItem.text, headlineItem.id, currentRootNode);
+            // if level is same, add to the current list
+        }
+        else if (headlineItem.level < prevListItem.level) {
+            console.log('level down', headlineItem.text, headlineItem.level, prevListItem.level, currentRootNode.text);
+            // if level is smaller, set current list to currentlist.parent
+            for (let i = 0; i < prevListItem.level - headlineItem.level; i++) {
+                currentRootNode = currentRootNode.parent;
+            }
+            prevListItem = addListItem(headlineItem.level, headlineItem.text, headlineItem.id, currentRootNode);
+        }
+    });
+
+    return toc;
+}
+
+/**
+ * Recursively turns a nested tree of tocItems to HTML.
+ * @param {TocItem} tocItem
+ * @returns {string}
+ */
+function tocItemToHtml(tocItem, listType) {
+    if (listType !== 'ol' && listType !== 'ul') {
+        listType = 'ul';
+    }
+    return '<' + listType + '>' + tocItem.children.map(childItem => {
+        let li = '<li>';
+        li += childItem.anchor
+            ? `<a href="#${childItem.anchor}">${childItem.text}</a>`
+            : childItem.text ? childItem.text : '';
+        return li + (childItem.children.length > 0 ? tocItemToHtml(childItem, listType) : '') + '</li>';
+    }).join('') + '</' + listType + '>';
+}
+
+function generateTocHtml(query, listType, doc = document) {
+    if (!query) {
+        query = 'h1, h2, h3, h4';
+    }
+    const headlineItems = findHeadlineElements(query, doc);
+    if (headlineItems?.length <= 0) {
+        return '';
+    }
+    const toc = flatHeadlineItemsToNestedTree(headlineItems);
+    const html = tocItemToHtml(toc, listType);
+    return html;
+}
+
+function findRenderTarget(element) {
+    if (element.hasAttribute('data-toc-render-target')) {
+        return element;
+    }
+    for (let child of element.children) {
+        const result = findRenderTarget(child);
+        if (result) {
+            return result;
+        }
+    }
+    return null;
+}
+
+class TableOfContents extends HTMLElement {
+
+    #initialInnerHTML;
+
+    connectedCallback() {
+        // store for re-creating the TOC just in case render() is called manually from outside
+        this.#initialInnerHTML = this.innerHTML;
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = this.#initialInnerHTML;
+        // store generated HTML in a template temporarily
+        const template = document.createElement('template');
+        template.innerHTML = generateTocHtml(this.getAttribute('selector'), this.getAttribute('listtype'));
+
+        // renderTarget mode: if this element has children, check if there is one with a special data attribute
+        // serving as anchor for where to place the list
+        const renderTarget = findRenderTarget(this);
+        if (renderTarget) {
+            renderTarget.appendChild(template.content.cloneNode(true));
+            return;
+        }
+
+        // default mode: append TOC at the end of its children (or empty body)
+        this.appendChild(template.content.cloneNode(true));
+    }
+}
+
+// define custom element only if it wasn't defined before
+if (! customElements.get('table-of-contents')) {
+    customElements.define('table-of-contents', TableOfContents);
+}
